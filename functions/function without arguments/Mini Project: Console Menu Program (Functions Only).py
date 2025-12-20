652c08fbe2fa98c402a29e41a837cb957111968d
musmanijaz305 <usmanijaz305@gmail.com>
2025-12-20 19:37:29 +0500

    Add console menu program using functions
    📌 Rules
    
    Use only functions
    No parameters yet
    No return values yet
    Use print() and function calls
    Simple and clean

---
+++ functions/function without arguments/Mini Project: Console Menu Program (Functions Only).py
@@ -0,0 +1,26 @@
+def header():
+  print("=====================")
+  print("PYTHON PROGRAM")
+  print("=====================")
+
+def show_menu():
+  print("1. About")
+  print("2. Help")
+  print("3. Exit")
+
+def about():
+  print("This program is created by Usman.")
+  print("It is a practice project for Python functions.")
+
+def help_menu():
+  print("Choose an option by entering a number.")
+  print("This is a simple console application.")
+
+def main():
+  header()
+  show_menu()
+  about()
+  help_menu()  
+  print("Program ended")
+
+main()
